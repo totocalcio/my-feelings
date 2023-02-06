f8f0e9d15e76a979d30677bceb82db9bc47de3f0
totocalcio <totohyper@yahoo.co.jp>
2023-02-06 22:12:59 +0900

    gpt-3連携

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,62 @@
 <script lang="ts" setup>
-const colorMode = useColorMode()
+const language = ref('日本語')
+const keyword = ref('')
+const length = ref(200)
+const feelings = ref('')
+const str = computed(
+  () => `
+    ${language.value}で回答して下さい。「${keyword.value}」のキーワードを元に、文章を作成して下さい。文章は${length.value}文字前後にして下さい。随所に次の言葉を使用してください「私は非常にがっかりしました」「腹ただしいです。」「我慢なりません」「改善を求めます」
+    `
+)
+const res = ref('')
 
-const themes = [
-  'system',
-  'light',
-  'dark',
-  'cupcake',
-  'bumblebee',
-  'emerald',
-  'corporate',
-  'synthwave',
-  'retro',
-  'cyberpunk',
-  'valentine',
-  'halloween',
-  'garden',
-  'forest',
-  'aqua',
-  'lofi',
-  'pastel',
-  'fantasy',
-  'wireframe',
-  'black',
-  'luxury',
-  'dracula',
-  'cmyk',
-  'autumn',
-  'business',
-  'acid',
-  'lemonade',
-  'night',
-  'coffee',
-  'winter',
-]
+const handleSubmit = async () => {
+  const prompt = str.value
+  const { data, error } = await useFetch(`/api/generate`, {
+    method: 'POST',
+    body: JSON.stringify({
+      prompt,
+    }),
+  })
+
+  if (error.value) {
+    throw new Error(error.value.data.message)
+  }
+  feelings.value = data.value.choices[0].text
+}
 </script>
 
 <template>
-  <div class="p-4 flex gap-4">
-    <button class="btn">Hello daisyUI</button>
-    <select class="select w-full max-w-xs" v-model="colorMode.preference">
-      <option disabled selected>Theme</option>
-      <option v-for="theme of themes" :key="theme">{{ theme }}</option>
-    </select>
-  </div>
-  <div class="hero bg-base-200 py-10">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">sample</p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
+  <div class="artboard artboard-demo h-screen p-10">
+    <h1 class="text-xl">お気持ち表明AI</h1>
+    <div class="flex flex-col gap-3 mt-3">
+      <label>
+        言語
+        <input type="text" class="input input-bordered" v-model="language" />
+      </label>
+      <label class="flex">
+        キーワード
+        <textarea
+          type="text"
+          class="textarea textarea-bordered w-60"
+          v-model="keyword"
+          placeholder=""
+        ></textarea>
+      </label>
+      <label>
+        文字数(目安)
+        <input type="number" class="input input-bordered" v-model="length" />
+      </label>
+      <button type="button" @click="handleSubmit" class="btn">
+        表明してもらう
+      </button>
     </div>
-  </div>
-  <div class="p-10">
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
+    <h2 class="text-lg mt-8">お気持ち表明</h2>
+    <div
+      class="mt-4 border-solid border-orange-500 border-4 p-5"
+      v-show="feelings"
+    >
+      {{ feelings }}
     </div>
   </div>
 </template>
